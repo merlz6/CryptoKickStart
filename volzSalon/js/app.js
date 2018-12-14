d716e68ebc1494cd70dec63413d47a23d642ac96
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2018-12-14 16:04:47 -0500

    prices hides homepage and shows services 'page'

--- volzSalon/js/app.js
+++ volzSalon/js/app.js
@@ -2,7 +2,11 @@ const app = angular.module('SalonApp', []);
 
 
 app.controller('MainController', ['$http', function($http){
+    this.showServices = false;
 
+    this.toggleShowServices = function(){
+      this.showServices = !this.showServices
+    }
 
 
   }])
