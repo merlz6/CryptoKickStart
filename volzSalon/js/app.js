ee6d0c9cbfccda7ea0d62efeffe3fd82509d17a2
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2018-12-15 12:56:26 -0500

    able to toggle contacts, home , services pages

--- volzSalon/js/app.js
+++ volzSalon/js/app.js
@@ -3,9 +3,23 @@ const app = angular.module('SalonApp', []);
 
 app.controller('MainController', ['$http', function($http){
     this.showServices = false;
+    this.showContacts = false;
+    this.showHomePage = true;
 
     this.toggleShowServices = function(){
-      this.showServices = !this.showServices
+      this.showServices = true;
+      this.showHomePage = false;
+      this.showContacts = false;
+    }
+    this.toggleContacts = function(){
+        this.showHomePage = false;
+      this.showServices = false;
+      this.showContacts = true;
+    }
+    this.toggleHome = function(){
+      this.showHomePage = true;
+      this.showServices = false;
+      this.showContacts = false;
     }
 
 
