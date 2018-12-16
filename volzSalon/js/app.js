5b109ea708a7bcd0f4f755ae5e0a2f41de5d9ab1
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2018-12-16 13:03:18 -0500

    mobile styling to home page, google maps and address added to contact page

--- volzSalon/js/app.js
+++ volzSalon/js/app.js
@@ -5,22 +5,33 @@ app.controller('MainController', ['$http', function($http){
     this.showServices = false;
     this.showContacts = false;
     this.showHomePage = true;
+    this.showPhotos = false;
 
     this.toggleShowServices = function(){
       this.showServices = true;
       this.showHomePage = false;
       this.showContacts = false;
+      this.showPhotos = false;
+
     }
     this.toggleContacts = function(){
         this.showHomePage = false;
       this.showServices = false;
       this.showContacts = true;
+      this.showPhotos = false;
+
     }
     this.toggleHome = function(){
       this.showHomePage = true;
       this.showServices = false;
       this.showContacts = false;
+      this.showPhotos = false;
     }
-
+    this.toggleContacts = function(){
+      this.showHomePage = false;
+      this.showServices = false;
+      this.showPhotos = false;
+      this.showContacts = true;
+    }
 
   }])
